84303725752ac88309b8b26b487bb4e72a690080
gauer10 <gauer10@gmail.com>
2018-10-07 19:26:00 +0200

    initial commit

---
+++ src/main/java/pl/wojtek/system_ksiegowy/SystemKsiegowyApplication.java
@@ -0,0 +1,12 @@
+package pl.wojtek.system_ksiegowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemKsiegowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemKsiegowyApplication.class, args);
+    }
+}
---
+++ src/main/java/pl/wojtek/system_ksiegowy/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package pl.wojtek.system_ksiegowy.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
